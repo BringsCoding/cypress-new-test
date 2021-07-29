df06bee0c8e6129aea9352e38ce3a5c87a77fca6
BringsCoding <simonbrings@googlemail.com>
2021-07-29 16:39:31 +0200

    Testing-fix

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
-const input = document.querySelector("#input");
-const addButton = document.querySelector("#add-button");
-const list = document.querySelector("#list");
+const input = document.querySelector('#input');
+const addButton = document.querySelector('#add-button');
+const list = document.querySelector('#list');
 
-addButton.addEventListener("click", addTodo);
+addButton.addEventListener('click', addTodo);
 
-function addTodo() {
+function addTodo () {
   const newTodo = input.value;
-  const newLi = document.createElement("li");
+  const newLi = document.createElement('li');
   newLi.innerText = newTodo;
 
   list.appendChild(newLi);
